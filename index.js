4b1f1e2868e889437e42cad1ea8de84f86998615
GiaVinhNguyen <vinh.g.nguyen19@gmail.com>
2023-12-11 23:40:49 -0500

    array's status: arranged

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+ function writeCards(nameArray, gift) {
+    const newArray = [];
+    for (let i = 0; i < nameArray.length; i++) {
+        newArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${gift} gift!`);
+    }
+    return newArray;
+ }
+
+ function countDown(number) {
+    while (number > -1) {
+        console.log(number);
+        number-=1;
+    }
+ }
+
+ writeCards(["bob", "john", "lisa"], "surprise");
+ countDown(10);
